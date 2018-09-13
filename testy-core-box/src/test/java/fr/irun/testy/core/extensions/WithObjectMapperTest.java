5937d6dd4764403bef1f6687bdcd8e633d688b11
Frédéric Combes <frederic.Combes@i-run.fr>
2018-09-13 11:52:55 +0200

    test(core): Remove legacy jsr310 module import

--- testy-core-box/src/test/java/fr/irun/testy/core/extensions/WithObjectMapperTest.java
+++ testy-core-box/src/test/java/fr/irun/testy/core/extensions/WithObjectMapperTest.java
@@ -1,7 +1,7 @@
 package fr.irun.testy.core.extensions;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import fr.irun.testy.core.dummy.Dummy;
 import fr.irun.testy.core.dummy.DummyMixin;
@@ -39,7 +39,7 @@ class WithObjectMapperTest {
         WithObjectMapper wMapper = WithObjectMapper.builder()
                 .dontFindAndRegisterModules()
                 .addModule(new ParameterNamesModule())
-                .addModule(new JSR310Module())
+                .addModule(new JavaTimeModule())
                 .addMixin(Dummy.class, DummyMixin.class)
                 .build();
 
@@ -47,7 +47,7 @@ class WithObjectMapperTest {
         void should_inject_object_mapper(ObjectMapper tested) {
             assertThat(tested.getRegisteredModuleIds()).containsOnly(
                     "com.fasterxml.jackson.module.paramnames.ParameterNamesModule",
-                    "com.fasterxml.jackson.datatype.jsr310.JSR310Module"
+                    "com.fasterxml.jackson.datatype.jsr310.JavaTimeModule"
             );
 
             assertThat(tested.findMixInClassFor(Dummy.class))
