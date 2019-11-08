5181c283df70b5ce955f91241a4ced0421f21a60
cdaleas <clement.daleas@i-run.fr>
2019-11-08 12:10:58 +0100

    chore(testy-beat-box / poms): Modify and add Javadoc, delete pom versions into sub-modules.
    closes nora/rocket/testy-box#3

--- testy-beat-box/src/main/java/fr/irun/testy/beat/extensions/WithRabbitMock.java
+++ testy-beat-box/src/main/java/fr/irun/testy/beat/extensions/WithRabbitMock.java
@@ -10,8 +10,6 @@ import org.junit.jupiter.api.extension.ExtensionContext.Store;
 import org.junit.jupiter.api.extension.ParameterContext;
 import org.junit.jupiter.api.extension.ParameterResolutionException;
 import org.junit.jupiter.api.extension.ParameterResolver;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
 import reactor.rabbitmq.ReceiverOptions;
@@ -28,10 +26,62 @@ import static fr.irun.testy.beat.messaging.AMQPHelper.declareSenderOptions;
 
 /**
  * Allow getting a Mock of a Rabbit channel in Tests. Building also Sender and Receiver Options.
+ * <p>
+ * Usage :
+ * <pre style="code">
+ *     {@literal @}RegisterExtension
+ *     WithRabbitMock wRabbitMock = WithRabbitMock.builder()
+ *             .declareQueueAndExchange("queue-name", "exchange-queue-name")
+ *             .declareReplyMessage("Reception OK. Reply Message !")
+ *             .build();
+ * </pre>
+ * <p>
+ * Usage without reply message (return null as reply message) :
+ * <pre style="code">
+ *     {@literal @}RegisterExtension
+ *     WithRabbitMock wRabbitMock = WithRabbitMock.builder()
+ *             .declareQueueAndExchange("queue-name", "exchange-queue-name")
+ *             .build();
+ * </pre>
+ * <p>
+ * Assert example to test a "listener" class :
+ * <pre style="code">
+ *     {@literal @}Test
+ *     void test_class_communication(SenderOptions senderOptions) {
+ *          Supplier&lt;String&gt; idGenerator = () -> "ID" + Math.random();
+ *          tested.subscribe();
+ *
+ *          assertThat(AMQPHelper.emitWithReply("message to send to tested", senderOptions, "exchange-queue-name", idGenerator)
+ *                 .flatMap(delivery -> Mono.fromCallable(() -> objectMapper.readValue(delivery.getBody(), String.class))).block())
+ *                 .isEqualTo("message received from tested");
+ *      }
+ * </pre>
+ * <p>
+ * Assert example to test a "sender / emitter" class :
+ * <pre style="code">
+ *     {@literal @}Test
+ *     void test_class_communication(Queue&lt;Delivery&gt; messagesReceived) throws IOException {
+ *
+ *         tested.execute();
+ *
+ *         assertThat(objectMapper.readValue(messagesReceived.remove().getBody(), String.class))
+ *                 .isEqualTo("message sent by tested");
+ *     }
+ *     <p>With a custom consumer</p>
+ *     {@literal @}Test
+ *     void test_class_communication(Channel channel) throws IOException {
+ *         Queue&lt;Delivery&gt; messagesReceived = new ArrayBlockingQueue&lt;&gt;(QUEUE_CAPACITY);
+ *         AMQPHelper.declareConsumer(channel, messagesReceived, "queue-name", "message received");
+ *
+ *         tested.execute();
+ *
+ *         assertThat(objectMapper.readValue(messagesReceived.remove().getBody(), String.class))
+ *                 .isEqualTo("message sent by tested");
+ *     }
+ * </pre>
  */
 public final class WithRabbitMock implements BeforeEachCallback, ParameterResolver {
     private static final int QUEUE_CAPACITY = 10;
-    private static final Logger LOGGER = LoggerFactory.getLogger(WithRabbitMock.class);
     private static final String P_RABBIT_CHANNEL = "rabbit-channel";
     private static final String P_RABBIT_SENDER_OPT = "rabbit-sender-opt";
     private static final String P_RABBIT_RECEIVER_OPT = "rabbit-receiver-opt";
@@ -102,33 +152,42 @@ public final class WithRabbitMock implements BeforeEachCallback, ParameterResolv
         throw new ParameterResolutionException("Unable to resolve parameter for Rabbit Channel !");
     }
 
-    public Channel getRabbitChannel(ExtensionContext context) {
-        return getStore(context).get(P_RABBIT_CHANNEL, Channel.class);
-    }
-
     private Store getStore(ExtensionContext context) {
         return context.getStore(ExtensionContext.Namespace.create(getClass()));
     }
 
+    /**
+     * Get the Rabbit Channel used for communication
+     *
+     * @param context The extension context useful for retrieving object into store
+     * @return The Channel created
+     */
+    public Channel getRabbitChannel(ExtensionContext context) {
+        return getStore(context).get(P_RABBIT_CHANNEL, Channel.class);
+    }
+
+    /**
+     * Get the Sender Options used for communication
+     *
+     * @param context The extension context useful for retrieving object into store
+     * @return The Sender Options used for channel creation
+     */
     public SenderOptions getSenderOptions(ExtensionContext context) {
         return getStore(context).get(P_RABBIT_SENDER_OPT, SenderOptions.class);
     }
 
+    /**
+     * Get the Receiver Options used for communication
+     *
+     * @param context The extension context useful for retrieving object into store
+     * @return The Receiver Options used for channel creation
+     */
     public ReceiverOptions getReceiverOptions(ExtensionContext context) {
         return getStore(context).get(P_RABBIT_RECEIVER_OPT, ReceiverOptions.class);
     }
 
     /**
      * Allow to build a Channel rabbit
-     * <p>
-     * Usage :
-     * <pre style="code">
-     *     {@literal @}RegisterExtension
-     *     WithRabbitMock wRabbitMock = WithRabbitMock.builder()
-     *             .declareQueueAndExchange("queue-name", "exchange-queue-name")
-     *             .declareReplyMessage("Reception OK. Reply Message !")
-     *             .build();
-     * </pre>
      */
     public static class WithRabbitMockBuilder {
 
