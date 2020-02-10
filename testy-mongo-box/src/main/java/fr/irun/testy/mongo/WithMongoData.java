1fa765ab31efcbd8e74a46594fbe3bbf0b101105
bneuenschwander <bertrand.neuenschwander@i-run.fr>
2020-02-10 08:58:54 +0100

    improve(testy-mongo-box): improve the injection of ObjectMapper extension in WithMongoData
    refs #6

--- testy-mongo-box/src/main/java/fr/irun/testy/mongo/WithMongoData.java
+++ testy-mongo-box/src/main/java/fr/irun/testy/mongo/WithMongoData.java
@@ -15,6 +15,7 @@ import org.reactivestreams.Subscriber;
 import org.reactivestreams.Subscription;
 import org.springframework.data.mongodb.ReactiveMongoDatabaseFactory;
 
+import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
 import java.util.List;
 import java.util.Map;
@@ -33,12 +34,20 @@ public final class WithMongoData implements BeforeEachCallback {
     private final Map<String, MongoDataSet<?>> dataSets;
 
     private WithMongoData(WithEmbeddedMongo wEmbeddedMongo,
-                          @Nullable WithObjectMapper wObjectMapper,
+                          Map<String, MongoDataSet<?>> dataSets) {
+        this.wEmbeddedMongo = wEmbeddedMongo;
+        this.wObjectMapper = null;
+        this.dataSets = dataSets;
+    }
+
+    private WithMongoData(WithEmbeddedMongo wEmbeddedMongo,
+                          @Nonnull WithObjectMapper wObjectMapper,
                           Map<String, MongoDataSet<?>> dataSets) {
         this.wEmbeddedMongo = wEmbeddedMongo;
         this.wObjectMapper = wObjectMapper;
         this.dataSets = dataSets;
     }
+
 
     @Override
     public void beforeEach(ExtensionContext context) {
@@ -127,7 +136,9 @@ public final class WithMongoData implements BeforeEachCallback {
          * @return The built {@link WithMongoData} extension.
          */
         public WithMongoData build() {
-            return new WithMongoData(wEmbeddedMongo, wObjectMapper, dataSetsBuilder.build());
+            return Optional.ofNullable(wObjectMapper)
+                    .map(wom -> new WithMongoData(wEmbeddedMongo, wom, dataSetsBuilder.build()))
+                    .orElseGet(() -> new WithMongoData(wEmbeddedMongo, dataSetsBuilder.build()));
         }
     }
 
