70b50b27045014641a6379b340ebec04bdade802
Marthym <marthym@gmail.com>
2023-10-29 14:49:27 +0100

    feat(params): #6 add unit test for StringVargsAggregator

--- testy-params-box/src/main/java/fr/ght1pc9kc/testy/params/aggregators/StringVargsAggregator.java
+++ testy-params-box/src/main/java/fr/ght1pc9kc/testy/params/aggregators/StringVargsAggregator.java
@@ -5,7 +5,21 @@ import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
 import org.junit.jupiter.params.aggregator.ArgumentsAggregationException;
 import org.junit.jupiter.params.aggregator.ArgumentsAggregator;
 
-public class VarargsAggregator implements ArgumentsAggregator {
+/**
+ * Allow to aggregate String Var Args in junit parameterized Test
+ *
+ * <pre style="java">
+ * {@literal @}ParameterizedTest
+ * {@literal @}CsvSource({
+ *        "1, one, two, three"
+ * })
+ * void should_use_string_aggregator(int index, @AggregateWith(StringVargsAggregator.class) String... aggregated) {
+ *     assertThat(index).isEqualTo(1);
+ *     assertThat(aggregated).containsExactly("one", "two", "three");
+ * }
+ * </pre>
+ */
+public class StringVargsAggregator implements ArgumentsAggregator {
     @Override
     public Object aggregateArguments(ArgumentsAccessor accessor, ParameterContext context) throws ArgumentsAggregationException {
         return accessor.toList().stream()
